9993fb34a3f0696ff02fb5a4995bddb53e9ef14a
rootikigai <ifykel360@gmail.com>
2025-05-04 16:56:21 +0100

    first commit for Fuctions tasks from Mr. Femi (He called 'em Munchies). Wrote function for returning multiples of an integer inputted.

---
+++ Munchies.java
@@ -0,0 +1,5 @@
+public class Munchies{
+	public static int findMultiple(int number){
+		return number * 5;
+	}
+}
---
+++ MunchiesTest.java
@@ -0,0 +1,5 @@
+public class MunchiesTest{
+	public static void main(String[] args){
+		System.out.println(Munchies.findMultiple(10));
+	}
+}
